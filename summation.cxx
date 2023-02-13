16a0519c16712d70a605ebb36b0728cd22584e30
Gayatripatra7844 <124420702+Gayatripatra7844@users.noreply.github.com>
2023-02-13 23:46:32 +0530

    Add files via upload

---
+++ summation.cxx
@@ -0,0 +1,12 @@
+//summation of two number in c
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	int sum;
+	printf("enter values of a & b:");
+	scanf("%d%d",&a ,&b);
+	sum=a+b;
+	printf("sum of a & b is:%d",sum);
+	return 0;
+}
